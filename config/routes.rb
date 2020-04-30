4b2d3638db8acd77a48264a5aa7ef4bfc8c6c7c3
vincentlss <shyanshinvincent@gmail.com>
2020-04-30 21:55:51 +0800

    added view indiv article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'pages', to: 'pages#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:show]
 end
