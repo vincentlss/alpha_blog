e4e2c1001371590cf58860298b439efad5bedd0a
vincentlss <shyanshinvincent@gmail.com>
2020-05-03 21:15:00 +0800

    Added users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,11 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @articles = @user.articles
-    end 
+    end
+    
+    def index
+        @users = User.all
+    end
 
     def new
         @user = User.new
