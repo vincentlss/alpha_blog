b9d42b86fec8483394079ab42503fd8b08028e88
vincentlss <shyanshinvincent@gmail.com>
2020-05-03 23:10:13 +0800

    Controller methods as helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,7 @@ module ApplicationHelper
         image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(image_src, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
+
+
+
 end
