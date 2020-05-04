f6b0b501087faa3f3eca143da8f333910b8ab024
vincentlss <shyanshinvincent@gmail.com>
2020-05-04 18:34:28 +0800

    Added integration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,27 @@
 class CategoriesController < ApplicationController
     def index
+        @categories = Category.all
     end
 
     def new
+        @category = Category.new
     end
 
     def show
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category was created successfull"
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
